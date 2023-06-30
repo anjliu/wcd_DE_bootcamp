cb234b323cae267b349e6d6ef51d9ffa243f5fc2
AJL <anjie.eijna@gmail.com>
2023-06-30 22:46:49 +0000

    update

--- chapter3/assignment3.1/get_data.py
+++ chapter3/assignment3.1/get_data.py
@@ -6,42 +6,42 @@ import boto3
 import toml
 import os
 
+if __name__=='__main__':
+    specs = toml.load('specs.toml') 
+    #%% get data
+    print('Downloading data...')
+    response = rq.get('https://www.themuse.com/api/public/jobs?page=50')
+    dt = response.text
+    dic = json.loads(dt)
+    print('Data downloaded.')
+    # %% convert to dataframe
+    print('Processing data...')
+    df = pd.json_normalize(dic['results'])
+    # split locations to city and state/country
+    df['city'] = df.locations.map(lambda x: x[-1]['name'].split(',')[0])
+    df['country'] = df.locations.map(lambda x: x[-1]['name'].split(',')[1])
+    df['date'] = pd.to_datetime(df.publication_date).dt.date
+    df = df[[
+        'company.name'
+        ,'country'
+        ,'city'
+        ,'name'
+        ,'type'
+        ,'date'
+    ]].rename({
+        'name':'job_name'
+        ,'type':'job_type'
+        ,'company.name':'company_name'
+    })
+    # %% export to csv
+    df.to_csv('jobs.csv')
+    print('Data processed and saved locally.')
+    #%% Upload to S3
+    # access_key=os.getenv('access_key')
+    # secret_access_key=os.getenv('secret_access_key')
+    bucket = specs['aws']['bucket']
+    folder = specs['aws']['folder']
+    s3 = boto3.client('s3')
+    s3.upload_file('jobs.csv', bucket, folder+'jobs.csv')
 
-specs = toml.load('specs.toml') 
-#%% get data
-print('Downloading data...')
-response = rq.get('https://www.themuse.com/api/public/jobs?page=51')
-dt = response.text
-dic = json.loads(dt)
-print('Data downloaded.')
-# %% convert to dataframe
-print('Processing data...')
-df = pd.json_normalize(dic['results'])
-# split locations to city and state/country
-df['city'] = df.locations.map(lambda x: x[-1]['name'].split(',')[0])
-df['country'] = df.locations.map(lambda x: x[-1]['name'].split(',')[1])
-df['date'] = pd.to_datetime(df.publication_date).dt.date
-df = df[[
-    'company.name'
-    ,'country'
-    ,'city'
-    ,'name'
-    ,'type'
-    ,'date'
-]].rename({
-    'name':'job_name'
-    ,'type':'job_type'
-    ,'company.name':'company_name'
-})
-# %% export to csv
-df.to_csv('jobs.csv')
-print('Data processed and saved locally.')
-#%% Upload to S3
-access_key=os.getenv('access_key')
-secret_access_key=os.getenv('secret_access_key')
-bucket = specs['aws']['bucket']
-folder = specs['aws']['folder']
-s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
-s3.upload_file('jobs.csv', bucket, folder+'jobs.csv')
-
-print('File uploaded.')
+    print('File uploaded.')
--- chapter3/assignment3.1/run.sh
+++ chapter3/assignment3.1/run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-export BASE_DIR = $(pwd)
-python3 ${BASE_DIR}
+export BASE_DIR=$(pwd)
+export PY_SCRIPT="get_data.py"
+
+cd ${BASE_DIR}
+
+echo "Run python script."
+python3 ${BASE_DIR}/${PY_SCRIPT}
+
+RESP=$?
+if [ ${RESP} != 0 ]; then
+	echo "Python script failed."
+	echo "ERROR: ${RESP}"
+	exit 1
+fi
+
+echo "Done!"
+
+exit 0
